d7930c624c58d7a7e960d78c01954fe245ed617c
Uğur Okan Çivgin <37384522+ugurcivgin34@users.noreply.github.com>
2022-05-14 23:59:30 +0300

    Vitrin sayfası tasarlanmaya başlandı

---
+++ MvcProjeUI/Controllers/DefaultController.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeUI.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+
+        HeadingManager hm = new HeadingManager(new EfHeadingDal());
+        public ActionResult Headings()
+        {
+            var headingList = hm.List();
+            return View(headingList);
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
